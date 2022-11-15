509a8a5bd6535e7badf3efe74dd01d00d3014e17
MX48@Live <MX48Live@Gmail.com>
2022-11-15 21:57:51 +0700

    Add Video Section

--- js/script.js
+++ js/script.js
@@ -1,20 +1,61 @@
-// Topbar Component
-$('.component-searchbar .search input').focus(() => {
-  $('.component-searchbar .search').addClass('active');
-})
-$('.component-searchbar .search input').blur(() => {
-  $('.component-searchbar .search').removeClass('active');
-})
-$('.component-searchbar .search form').submit(function (e) {
-  e.preventDefault();
-  var searchValue = $('.component-searchbar .search input').val()
-  function searchQuery() {
-    window.location.href = `/?s=${searchValue}`
-  }
-  searchQuery()
-})
+$(document).ready(function () { 
+    
+  // Topbar Component
+  $('.component-searchbar .search input').focus(() => {
+    $('.component-searchbar .search').addClass('active');
+  })
+  $('.component-searchbar .search input').blur(() => {
+    $('.component-searchbar .search').removeClass('active');
+  })
+  $('.component-searchbar .search form').submit(function (e) {
+    e.preventDefault();
+    var searchValue = $('.component-searchbar .search input').val()
+    function searchQuery() {
+      window.location.href = `/?s=${searchValue}`
+    }
+    searchQuery()
+  })
 
-// Menubar Component
-$('.component-menubar .menu-mobile-toggle').on('click', function () {
-  $('.component-menubar .menu-container').toggleClass('open')
+  // Menubar Component
+  $('.component-menubar .menu-mobile-toggle').on('click', function () {
+    $('.component-menubar .menu-container').toggleClass('open')
+  })
+
+  $('.component-menubar .menu-container li a').on('click', function () {
+    $('.component-menubar .menu-container').removeClass('open')
+  })
+
+
+  // Video Ccarousel
+  $('.video-carousel-container').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: `<img src="/images/slick/slick-next.png" class="next" alt="Next" />`,
+    prevArrow: `<img src="/images/slick/slick-prev.png" class="prev" alt="Next" />`,
+    mobileFirst: true,
+    variableWidth: true,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          variableWidth: false,
+          arrows: true,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          variableWidth: false,
+          arrows: true,
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+    ]
+  })
+
 })
